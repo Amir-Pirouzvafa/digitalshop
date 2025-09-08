7049b51a6ea8870a8e9dd7f5ea51bdc7ed4eb1ff
amir_mohammad <amirp458@gmail.com>
2025-09-08 16:16:54 +0330

    add tailwind and shadCN

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import Banner from "@/components/banner";
+import Welcome from "@/components/Welcome";
+
 export default function Home() {
-  const renderName = () => {
-    const name = 'Ali';
-    return <span>{name}</span>;
-  };
+
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>my first page</div>
-      {renderName()}
+    <div className="flex flex-col items-center justify-center h-screen">
+        <Banner />
+        <Welcome />
     </div>
   );
 }
